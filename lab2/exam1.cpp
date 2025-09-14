df2af584623d1bb79868a03c02d00de9a52d2eb3
Shinebayar Tuvshinsaikhan <tuvshinsaikhan@Shinebayars-MacBook-Pro.local>
2025-09-14 23:46:29 +0800

    lab2 completed

--- lab2/exam1.cpp
+++ lab2/exam1.cpp
@@ -1,30 +1,20 @@
 #include <iostream>
 using namespace std
 ;
-// 4. Өгөгдсөн N хүртэлх тооны нийлбэр, арифметик дундажыг ол.
-
-float FindAverage(int array[], int N)
-{
-    int sum = 0;
-    for (int i = 0; i < N; i++)
-    {
-        sum += array[i];
-    }
-    return sum / N; 
-}
+// 1. a, b, c гурван тооны хамгийн ихийг олж дэлгэцэнд хэвэл.
 
 int main()
 {
-    int N;
-    cin >> N;
-    int array[N];
-    for (int i = 0; i < N; i++)
-    {
-        cin >> array[i];
-    }
-    float averageNumber = FindAverage(array, N);
-    cout << averageNumber << endl;
+    int a, b, c;
+    cin >> a >> b >> c;
+
+    // a, b тоог жишээд хэрвээ b нь их бол а-д b-ийн утгыг өгнө. Үгүй бол b нь их гэсэн үг. 
+    // Дараа нь a, c-г жишээд с нь их бол а-д c-ийн утгыг өгөөд а-г хэвлэнэ.
+    if(b>a) a = b; 
+    if(c>a) a = c;
+
+    cout << "хамгийн их тоо = " << a << endl;
     return 0;
 }
-// g++ exam4.cpp -o exam4
-// ./exam4
+// g++ exam1.cpp -o exam1
+// ./exam1
